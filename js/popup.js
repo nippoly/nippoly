0f8f8f66a5199a7c2daae8d24aeca12254fe9c7b
うーぴょん ᕱ⑅ᕱ <sasurai-usagi3@users.noreply.github.com>
2017-04-13 22:51:02 +0900

    フロントとのすり合わせ (#26)

--- js/popup.js
+++ js/popup.js
@@ -1,28 +1,36 @@
 window.addEventListener('load', () => {
   // TODO: delete when clear method implemented
   //chrome.storage.sync.clear();
-  // TODO: split when button implemented
-  new Promise((resolve, reject) => {
-    chrome.tabs.getSelected(null, resolve);
-  }).then((tab) => new Promise((resolve, reject) => {
-    chrome.storage.sync.get('data', (items) => resolve([items, tab]));
-  })).then((args) => {
-    let items = args[0];
-    let tab = args[1];
-    items.data = items.data || [];
-    items.data.push({title: tab.title, url: tab.url});
-    chrome.storage.sync.set(items, () => {
-      let status = document.getElementById('status-bar');
-      status.textContent = 'DONE'
-    });
+  let cancelBtn = document.getElementById('cancel');
+  let okBtn = document.getElementById('ok');
+  cancelBtn.addEventListener('click', function (e) {
+    window.close();
   });
-  // TODO: split when button implemented
-  chrome.storage.sync.get('data', (items) => {
-    const textArea = document.getElementById('test');
-    textArea.textContent = items.data.reduce((result, item) => {
-      return `${result}[${item.title}](${item.url})\n`
-    }, '');
-    textArea.select();
-    document.execCommand('copy');
+  okBtn.addEventListener('click', function (e) {
+    new Promise((resolve, reject) => {
+      chrome.tabs.getSelected(null, resolve);
+    }).then((tab) => new Promise((resolve, reject) => {
+      chrome.storage.sync.get('data', (items) => resolve([items, tab]));
+    })).then((args) => new Promise((resolve, reject) => {
+      let items = args[0];
+      let tab = args[1];
+      items.data = items.data || [];
+      items.data.push({title: tab.title, url: tab.url});
+      chrome.storage.sync.set(items, () => {
+        let status = document.getElementById('status-bar');
+        status.textContent = 'DONE'
+        resolve();
+      })
+    })).then(() => {
+      chrome.storage.sync.get('data', (items) => {
+        const textArea = document.getElementById('test');
+        textArea.textContent = items.data.reduce((result, item) => {
+          return `${result}[${item.title}](${item.url})\n`
+        }, '');
+        textArea.select();
+        document.execCommand('copy');
+        window.close();
+      });
+    });
   });
 });
