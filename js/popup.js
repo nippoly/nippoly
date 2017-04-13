b34f5aa4eed0bb8d3510c83e76b4b558e07e17ee
うーぴょん ᕱ⑅ᕱ <sasurai-usagi3@users.noreply.github.com>
2017-04-13 21:49:06 +0900

    storageを使えるように設定 (#20)

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,18 @@
 window.addEventListener('load', () => {
-  chrome.tabs.getSelected(null, (tab) => {
-    // FIXME: temporary code
-    let url = document.getElementById('page-url');
-    let title = document.getElementById('page-title');
-    url.textContent = tab.url;
-    title.textContent = tab.title;
-  });
+  // TODO: delete when clear method implemented
+  //chrome.storage.sync.clear();
+  new Promise((resolve, reject) => {
+    chrome.tabs.getSelected(null, resolve);
+  }).then((tab) => new Promise((resolve, reject) => {
+    chrome.storage.sync.get('data', (items) => resolve([items, tab]));
+  })).then((args) => {
+    let items = args[0];
+    let tab = args[1];
+    items.data = items.data || [];
+    items.data.push({title: tab.title, url: tab.url});
+    chrome.storage.sync.set(items, () => {
+      let status = document.getElementById('status-bar');
+      status.textContent = 'DONE'
+    });
+  })
 });
