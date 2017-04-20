75b7ab434eff9a2b93e1d6e10c999e292ab7873c
Fujii Yosuke <fujiyou1121@gmail.com>
2017-04-20 13:29:52 +0900

    Fixed #31 (#34)
    * add copy button(試作段階)
    
    * 一個のときはリスト表記にしない

--- js/popup.js
+++ js/popup.js
@@ -1,15 +1,19 @@
 window.addEventListener('load', () => {
   let cancelBtn = document.getElementById("cancel");
   let okBtn = document.getElementById("ok");
+  let copyBtn = document.getElementById("copys");
   let pageTitle = document.getElementById("page-title");
   let pageUrl = document.getElementById("page-url");
+
   chrome.tabs.getSelected(null, (tab) => {
     pageTitle.value = tab.title;
     pageUrl.textContent = tab.url;
   });
+
   cancelBtn.addEventListener('click', function (e) {
     window.close();
   });
+
   okBtn.addEventListener('click', function (e) {
     new Promise((resolve, reject) => {
       chrome.storage.sync.get('data', resolve);
@@ -18,19 +22,31 @@ window.addEventListener('load', () => {
       items.data.push({title: pageTitle.value, url: pageUrl.textContent, created_at: new Date().toString()});
       chrome.storage.sync.set(items, () => {
         let status = document.getElementById('status-bar');
-        status.textContent = 'DONE'
+        status.textContent = '追加しました。'
         resolve();
       });
     })).then(() => {
       chrome.storage.sync.get('data', (items) => {
         const textArea = document.getElementById('test');
-        textArea.textContent = items.data.reduce((result, item) => {
-          return `${result}[${item.title}](${item.url})\n`
-        }, '');
+        const item = items.data[items.data.length - 1];
+        textArea.textContent = `[${item.title}](${item.url})\n`
         textArea.select();
         document.execCommand('copy');
-        //window.close();
+        window.close();
       });
+    });
+  });
+
+
+  copyBtn.addEventListener('click', function (e) {
+    chrome.storage.sync.get('data', (items) => {
+      const textArea = document.getElementById('test');
+      textArea.textContent = items.data.reduce((result, item) => {
+        return `${result}- [${item.title}](${item.url})\n`
+      }, '');
+      textArea.select();
+      document.execCommand('copy');
+      window.close();
     });
   });
 
