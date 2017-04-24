492bd297231e3fb3a2ea04fd32da6b34e89cd0c6
うーぴょん ᕱ⑅ᕱ <sasurai-usagi3@users.noreply.github.com>
2017-04-24 18:07:16 +0900

    Update popup.js (#42)

--- js/popup.js
+++ js/popup.js
@@ -18,7 +18,7 @@ window.addEventListener('load', () => {
     new Promise((resolve, reject) => {
       chrome.storage.sync.get('data', resolve);
     }).then((items) => new Promise((resolve, reject) => {
-      items.data = (items.data || []).filter(item => new Date(item.created_at) >= new Date(Date.now() - 60 * 1000));
+      items.data = (items.data || []).filter(item => new Date(item.created_at) >= new Date(Date.now() - 24 * 3600 * 1000));
       items.data.push({title: pageTitle.value, url: pageUrl.textContent, created_at: new Date().toString()});
       chrome.storage.sync.set(items, () => {
         // let status = document.getElementById('status-bar');
